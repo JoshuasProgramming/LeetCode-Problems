2975049c151bc2734101423b8ea8977f20e90456
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-02-24 12:38:32 +0000

    Odd numbers from 99 to 1
    This isn't necessarily a leetcode question but, one which you could be questioned on.

---
+++ 99to1.js
@@ -0,0 +1,15 @@
+//Output all odd numbers from 99 to 1
+
+//loop through the array with 99 as your starting point, and 1 as your
+//destination point
+for(let i = 99; i >= 1; i--){
+    
+    //check if i is an odd number
+    if(i % 2 == 1){
+        
+        //when i is an odd number, output the number
+        console.log(i);
+    }
+    
+    //carry on doing this until i equals to 1
+}
